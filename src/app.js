a3e65f47ec5932af96dad31d2f131b15e3e9ae9a
HeymerDev <heymermeza11@gmail.com>
2023-11-22 21:58:11 -0500

    agregando CORS

--- src/app.js
+++ src/app.js
@@ -6,6 +6,14 @@ import userRouter from "./routes/user.routes.js";
 
 const app = express();
 app.use(express.json());
+// Middleware para permitir CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173"); // Reemplaza con tu dominio de frontend
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
 app.use("/api", productosRoutes);
 app.use("/api", ventasRoutes);
 app.use("/api", userRouter);
--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ config();
 export const PORT = process.env.PORT || 1005;
 export const DB_DATABASE = process.env.DB_DATABASE || "gestion";
 export const DB_USER = process.env.DB_USER || "root";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "root";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "Losmenores9724@";
 export const BD_PORT = process.env.BD_PORT || 3306;
 export const DB_HOST = process.env.DB_HOST || "localhost";
